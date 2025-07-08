024ca7f89e46a31e9777355b83c456371fa56b6c
ramazan3449 <ramazan.celik@agu.edu.tr>
2025-07-08 14:04:54 +0300

    Initial Django commit

--- backend/settings.py
+++ backend/settings.py
@@ -1,5 +1,6 @@
 import os
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -70,10 +71,7 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # SQLite for development, you can switch to PostgreSQL in production by setting DATABASE_URL env variable and using dj_database_url
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
 }
 
 # Password validation
